a0e6bb780c482bc568145e8bb3a085b11f40b18e
kirito <rssdevoloper@gmail.com>
2022-10-18 14:09:19 +0700

    make

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -3,7 +3,7 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
-    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, { asSticker: true })
     throw stiker.toString()
 }
 handler.help = ['ttp <teks>']
